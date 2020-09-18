d0990d62bcef982fcb6ce369c67040619fcf1333
harry <enesdyaz@gmail.com>
2020-09-18 12:11:57 +0800

    responsive index page

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,68 +1,111 @@
 <template>
-  <div>
-      <div :class='{active: !isActive}' class='leftBox'>a</div>
-      <div :class='{active: isActive}' class='rightBox'>b</div>
-      <div class='bottomNavigation d-sm-none'>
-          <div class='bottomAdmin'><v-btn text @click='admin'>ADMIN</v-btn> </div>
-          <div class='bottomView'><v-btn text @click='view' > VIEW</v-btn></div>
-      </div>
-  </div>
+    <v-app>
+
+<!--- Drawer -->
+    <v-navigation-drawer v-model="drawer" right fixed app dense>
+        <v-list>
+        <v-container class='d-flex justify-center'>
+            <img width='100' alt="Vuetify Logo" :src="profileImage" >
+        </v-container>
+    <br>
+        <v-subheader>ADMIN PANEL</v-subheader>
+        <v-divider></v-divider>
+        <v-list-item v-for="(item, i) in items" :key="i" :to="item.to" router exact dense >
+
+            <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-action>
+
+            <v-list-item-content style='margin-left: -20px'>
+            <v-list-item-title v-text="item.title" />
+            </v-list-item-content>
+
+        </v-list-item>
+        </v-list>
+    </v-navigation-drawer>
+
+
+<!-- header --> 
+        <header class='headerContent'>
+
+            <div class='headerMenu d-flex pa-4'>
+                <div><v-img src='/default.png' width='30'></v-img></div>
+                <div class='text-button pl-2' style='color: #546E7A' >wizeto</div>
+                <v-spacer></v-spacer>
+                <div class='d-none d-sm-flex mt-2'><v-btn small text> Documentation</v-btn></div>
+                <div class='d-none d-sm-flex mt-2'><v-btn small text> Product</v-btn></div>
+                <div class='d-none d-sm-flex mt-2'><v-btn small text> Pricing</v-btn></div>
+                <div class='d-none d-sm-flex mt-2'><v-btn small text> </v-btn></div>
+
+                <div><v-btn text outlined color='green'> Signup</v-btn></div>
+                <div><v-btn text to='/setup'> Login</v-btn></div>
+                <div class='d-sm-none'><v-icon @click.stop="drawer = !drawer" right style='margin-top: 6px;'>mdi-menu</v-icon></div>
+
+            </div>
+
+            <section class='headerSection1'>
+              <v-row no-gutters>
+                <v-col cols='12' sm='6'>
+                    <div class='section1Left' style='padding: 10%;' >
+                        <div class='text-h4 mt-sm-4 font-weight-bold'>All you need to do is show up at the right time.</div>
+                        <br>
+                        <div class='text-lg-h6 font-weight-medium'>Wizeto is your online assistant, working 24/7 to fill your booking, schedule, all you need.</div>
+                    </div>
+                </v-col>
+                <v-col cols='12' sm='6'>
+                    <div class='section1Right mt-sm-16 mt-md-0' style='padding: 10%;'>
+                      <v-img src='/screenShot.png'></v-img>
+                    </div>
+                </v-col>
+              </v-row>
+            </section>
+        </header>
+        
+ 
+
+
+
+        <div><nuxt /></div>
+    </v-app>
 </template>
 
 <script>
 export default {
-  data() {
+layout: 'landing',
+data () {
     return {
-      isActive: true
+    drawer: false,
+    profileImage: '/v.png',
+    items: [
+        {
+        icon: 'mdi-chevron-down',
+        title: 'Home',
+        to: '/'
+        },
+        {
+        icon: 'mdi-chevron-down',
+        title: 'Service',
+        to: '/inspire'
+        }
+    ],
+
+    title: 'Wizeto'
     }
-  },
-  methods: {
-    admin(){
-      this.isActive = true;
-    },
-    view(){
-      this.isActive = false
-    }
-  }
+}
 }
 </script>
 
 <style lang='scss' scoped>
-
-@media only screen and (max-width: 600px) {
-  body {
-    background-color: lightblue;
-  }
-  .active{
-  display: none;
-  }
+.headerContent{
+    background: #FFEBEE;
 }
-
-
-.leftBox{
-  width: 50%;
-  float: left;
+.headerSection1{
+    
+    .section1Left{
+        
+    }
+    .section1Right{
+       @extend .section1Left;
+    }
 }
-.rightBox{
-  width: 50%;
-  float: left;
-}
-
-.bottomNavigation{
-  position: fixed;
-  bottom: 0;
-  width: 100%;
-  
-  .bottomAdmin{
-    float: left;
-    width: 50%;
-    border: 1px solid whitesmoke;
-    text-align: center;
-  }
-  .bottomView{
-  @extend .bottomAdmin
-  }
-}
-
-
 </style>
